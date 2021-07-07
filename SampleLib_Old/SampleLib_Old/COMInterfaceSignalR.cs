068b8317c579fc9994cb40633a210f5bd21de4b2
borap90 <52690005+borap90@users.noreply.github.com>
2021-07-07 19:20:40 +0530

    Add files via upload
    SampleLib_Old

---
+++ SampleLib_Old/SampleLib_Old/COMInterfaceSignalR.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SampleLib_Old
+{
+    [Guid("06FACA96-1FCD-4A91-95DB-D644705BE578")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    public interface COMInterfaceSignalR
+    {
+        [DispId(1)]
+        void Fire_PrintComplete();
+    }
+}
---
+++ SampleLib_Old/SampleLib_Old/SignalRHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Runtime.InteropServices;
+
+namespace SampleLib_Old
+{
+    [Guid("5BA12413-97FF-4181-99A4-0A8849FF725E")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ProgId("TestLibrary.Program")]
+    public class SignalRHub : Hub, COMInterfaceSignalR
+    {
+        public void Fire_PrintComplete()
+        {
+            try
+            {
+                var context = GlobalHost.ConnectionManager.
+                              GetHubContext<SignalRHub>();
+                context.Clients.All.PrintComplete();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
